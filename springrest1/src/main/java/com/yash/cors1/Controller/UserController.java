719c6532e5877f0b490007be09455fa28fa865d3
shahimsha.shaik <shahimsha.shaik@YH1005554DT.Yash.com>
2018-08-07 18:52:24 +0530

    changes in usercontroller

--- springrest1/src/main/java/com/yash/cors1/Controller/UserController.java
+++ springrest1/src/main/java/com/yash/cors1/Controller/UserController.java
@@ -1,14 +1,17 @@
 package com.yash.cors1.Controller;
 
+import java.util.Map;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:7070", methods = RequestMethod.PUT)
+//@CrossOrigin(origins = "http://localhost:7070", methods = RequestMethod.PUT)
 @RestController
 public class UserController {
 
@@ -30,4 +33,43 @@ public class UserController {
 		return user;
 	}
 
+	@RequestMapping(value = "/update", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User add(@RequestBody User user){
+		user.setName(user.getName() + "shaik pavani");
+		System.out.println("rest call pavani");
+		return user;
+		
+	}
+	@RequestMapping(value = "/update", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User get(){
+		User user=new User();
+		user.setId(111);
+		user.setName("tanu ja" + "shaik ");
+		System.out.println("rest call");
+		return user;
+		
+	}
+	
+	
+	
+	@RequestMapping(value="/getuser/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User method1(@RequestParam("id") int id ){
+		User user=new User();
+		user.setId(id);
+		user.setName("abc");
+		return user;
+		
+	}
+	@RequestMapping(value="/getuser/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User method2( @RequestParam Map<String ,String> map){
+		User user=new User();
+		System.out.println("welcome multiple data"+map.get("state"));
+		user.setId(Integer.parseInt(map.get("id")));
+		user.setName(map.get("name"));
+		return user;
+		
+	}
+	
+	
+	
 }
