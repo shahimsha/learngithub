9ece9e388282a78f8d1bc9258a53d78e0eb17d17
shahimsha.shaik <shahimsha.shaik@YH1005554DT.Yash.com>
2018-08-13 15:10:55 +0530

    commit

--- springrest1/src/main/java/com/yash/cors1/Controller/Students.java
+++ springrest1/src/main/java/com/yash/cors1/Controller/Students.java
@@ -2,13 +2,6 @@ package com.yash.cors1.Controller;
 
 public class Students {
 
-	private String namedileep;
-	private String address;
-    private String state;
-    private String testcase;
-
-    private int id;
-
-	private String school;
+	private String name;
 	
 }
