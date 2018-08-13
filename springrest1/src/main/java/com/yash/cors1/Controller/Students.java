056ec2ed9ae051a7c52c31a53d8cd542ad8601f4
shahimsha.shaik <shahimsha.shaik@YH1005554DT.Yash.com>
2018-08-13 17:44:48 +0530

    delete age

--- springrest1/src/main/java/com/yash/cors1/Controller/Students.java
+++ springrest1/src/main/java/com/yash/cors1/Controller/Students.java
@@ -3,10 +3,7 @@ package com.yash.cors1.Controller;
 public class Students {
 
 	private String name;
-        private String year;
-	private String branch;
-
-        private String age;
+       
 
 	
 }
