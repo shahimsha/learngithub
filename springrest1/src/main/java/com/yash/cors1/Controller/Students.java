8e66da2e78e02986a4c5f4c914d6cf738c8a99af
shahimsha.shaik <shahimsha.shaik@YH1005554DT.Yash.com>
2018-08-13 15:46:17 +0530

    save name

--- springrest1/src/main/java/com/yash/cors1/Controller/Students.java
+++ springrest1/src/main/java/com/yash/cors1/Controller/Students.java
@@ -3,10 +3,7 @@ package com.yash.cors1.Controller;
 public class Students {
 
 	private String name;
-	private String address;
-	private String id;
-	private String state;
-	private String dist;
+	
 
 	
 }
