9de6f848f1e2e43ea0c33bc8805636106ce5114f
shahimsha <37866891+shahimsha@users.noreply.github.com>
2018-09-10 19:57:40 +0530

    Update PersonDTO.java
    state added

--- springrest1/src/main/java/com/yash/cors1/Controller/PersonDTO.java
+++ springrest1/src/main/java/com/yash/cors1/Controller/PersonDTO.java
@@ -4,27 +4,7 @@ public class PersonDTO {
 	
 	private String firstName;
 	private String secondName;
-	 private String first;
-	private String newfield;
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getSecondName() {
-		return secondName;
-	}
-	public void setSecondName(String secondName) {
-		this.secondName = secondName;
-	}
-	public String getNewfield() {
-		return newfield;
-	}
-	public void setNewfield(String newfield) {
-		this.newfield = newfield;
-	}
-
+	private String state;
 	
 
 	
