3a997ac5c69263527ee924aad6e4b57cedd89bd6
shahimsha.shaik <shahimsha.shaik@YH1005554DT.Yash.com>
2018-09-10 19:45:09 +0530

    commit to local repository

--- springrest1/src/main/java/com/yash/cors1/Controller/PersonDTO.java
+++ springrest1/src/main/java/com/yash/cors1/Controller/PersonDTO.java
@@ -1,17 +1,28 @@
 package com.yash.cors1.Controller;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class PersonDTO {
 	
 	private String firstName;
-	  private String secondName;
-	  private String salary;
-	private String branch;
+	 private String secondName;
+	private String newfield;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	public String getNewfield() {
+		return newfield;
+	}
+	public void setNewfield(String newfield) {
+		this.newfield = newfield;
+	}
+	
 
 }
